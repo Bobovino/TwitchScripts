717e5c3fa53822cdea0a482b51a29324345b3971
Bobovino <rodrigomartineztabernero97@gmail.com>
2023-12-05 17:28:45 +0100

    Final version

--- Click.js
+++ Click.js
@@ -1,5 +1,5 @@
 function clickButtonByXPath(xpath) {
-    const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+    const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE)
     const button = result.singleNodeValue
     if (button) {
         button.click()
@@ -8,7 +8,7 @@ function clickButtonByXPath(xpath) {
     }
 }
 
-// Repeat the process 85 times with a delay
+// Repeat the process 2 times with a 500s delay
 let counter = 0 
 const interval = setInterval(() => {
     clickButtonByXPath('//*[@id="live-page-chat"]/div/div/div[2]/div/div/section/div/seventv-container/div/div[2]/div[2]/div[1]/div/div/div/div[1]/div[2]/button')
@@ -16,7 +16,7 @@ const interval = setInterval(() => {
     clickButtonByXPath('//*[@id="channel-points-reward-center-body"]/div/div/div[3]/div/button')
 
     counter++
-    if (counter >= 3) {
+    if (counter >= 1) {
         clearInterval(interval)
     }
 }, 500) 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TwitchScripts
+1) Redeem n times some twitch channel points
